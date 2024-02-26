afec6911ebca2fbb4bdbd57ed8ce2a1dd6c86fed
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-26 18:29:43 -0500

    feat: include new loader component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,6 @@
-
-
 const Button = ({ text, onClick }) => {
   return (
-    <button 
+    <button
       onClick={onClick}
       className="bg-lime-500 py-2 w-44 absolute -bottom-24 right-0 text-lg rounded-3xl text-white mt-10 md:mt-4 md:-bottom-16 mg:bg-transparent mg:text-black mg:-bottom-16 sm:-right-8 sm:w-36"
     >
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const PokemonCard = ({ pokemon }) => {
   return (
@@ -26,7 +26,7 @@ const PokemonCard = ({ pokemon }) => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
