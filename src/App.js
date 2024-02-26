1a80bd7f7fd84cebad3ebdcb44d2a34b1499e99e
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-26 18:30:42 -0500

    feat: change spanish text to english and delete some lines

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Layout from './components/Layout';
 import Input from './components/Input';
 import Button from './components/Button';
 import PokemonCard from './components/PokemonCard';
+import Loader from './components/Loader';
 
 function App() {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
   const [searchParams, setSearchParams] = useState('');
-  const [status, setStatus] = useState('default')
+  const [status, setStatus] = useState('default');
 
   const isDefault = status === 'default';
   const isLoading = status === 'loading';
@@ -15,14 +16,13 @@ function App() {
   const isError = status === 'error';
 
   const handleSearchParams = (e) => {
-    setSearchParams(e.target.value)
+    setSearchParams(e.target.value);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!searchParams) {
-      return;
-    }
+
+    if (!searchParams) return;
     setStatus('loading');
     fetch(`https://pokeapi.co/api/v2/pokemon/${searchParams}`)
       .then(response => response.json())
@@ -34,8 +34,8 @@ function App() {
         console.error(error);
         setStatus('error');
         setSelectedPokemon(null);
-      })
-  }
+      });
+  };
 
   return (
     <Layout>
@@ -43,15 +43,15 @@ function App() {
         <h1 className='text-center text-white font-bold text-3xl mb-10 mg:my-6 md:my-4'>PokeDex</h1>
         <form onSubmit={e => handleSubmit(e)} className="flex flex-col">
           <Input type='number' placeholder="ID" onChange={e => handleSearchParams(e)} />
-          <Input type='text' placeholder="Nombre" onChange={e => handleSearchParams(e)} />
-          <Button onClick={(e) => handleSubmit(e)} text="Consultar" />
+          <Input type='text' placeholder="Name" onChange={e => handleSearchParams(e)} />
+          <Button onClick={(e) => handleSubmit(e)} text="Search" />
         </form>
       </div>
 
-      {isDefault && <h1 className='text-white w-2/5 text-2xl font-bold mg:py-4 md:w-11/12'>Ingresa tu búsqueda por nombre o ID</h1>}
+      {isDefault && <h1 className='text-white w-2/5 text-2xl font-bold mg:py-4 md:w-11/12'>Enter your search by name o ID</h1>}
       {isDone && <PokemonCard pokemon={selectedPokemon} />}
-      {isLoading && <h1 className='text-white text-3xl font-bold'>Cargando los datos del Pokemon</h1>}
-      {isError && <h1 className='text-white text-xl font-bold'>Ningún Pokemon coincide con tu búsqueda.</h1>}
+      {isLoading && <Loader />}
+      {isError && <h1 className='text-white text-xl font-bold'>No Pokemon match your search.</h1>}
     </Layout>
   );
 }
